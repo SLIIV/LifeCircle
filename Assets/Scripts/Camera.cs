be70a77a29766e9017adbcf9676fc8c89cb7c20c
SLIIV <YTANTEI@yandex.ru>
2021-05-17 21:16:12 +0900

    Настроена сцена, прикручено управление

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Camera : MonoBehaviour
+{
+    [SerializeField]
+    private float sensivity;
+
+    private Transform camera;
+    float mouseXDir;
+    float mouseYDir;
+
+    void Start() 
+    {
+        camera = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseXDir += Input.GetAxis("Mouse X") * sensivity;
+        mouseYDir += Input.GetAxis("Mouse Y") * sensivity;
+        mouseYDir = Mathf.Clamp(mouseYDir, -90, 90);
+
+        Vector3 cameraRot = new Vector3(-mouseYDir, mouseXDir, 0);
+
+        //gameObject.transform.localEulerAngles = new Vector3(0, cameraRot.y, 0);
+        camera.eulerAngles = cameraRot;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Transform playerCamera;
+
+    private CharacterController controller;
+
+    private Transform playerTransform;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardMovement = Input.GetAxisRaw("Vertical");
+        float rightMovement = Input.GetAxisRaw("Horizontal");
+        Vector3 newForwardVector = new Vector3(playerCamera.forward.x, 0, playerCamera.forward.z) * forwardMovement;
+        Vector3 newRightVector = new Vector3(playerCamera.right.x, 0, playerCamera.right.z) * rightMovement;
+        controller.Move((newForwardVector + newRightVector).normalized * speed * Time.deltaTime);
+    }
+}
